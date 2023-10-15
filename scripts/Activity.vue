62cde10f4c16879239d54b5cb200b3a03a63bb70
Alessandro Rezzi <alessandrorezzi2000@gmail.com>
2023-10-15 22:06:55 +0200

    fix confirmed balance again

--- scripts/Activity.vue
+++ scripts/Activity.vue
@@ -163,9 +163,8 @@ async function parseTXs(arrTXs) {
 
         // Coinbase Transactions (rewards) require coinbaseMaturity confs
         const fConfirmed =
-            cNet.cachedBlockCount - cTx.blockHeight >= props.rewards
-                ? cChainParams.current.coinbaseMaturity
-                : 6;
+            cNet.cachedBlockCount - cTx.blockHeight >=
+            (props.rewards ? cChainParams.current.coinbaseMaturity : 6);
 
         // Choose the content type, for the Dashboard; use a generative description, otherwise, a TX-ID
         // let txContent = props.rewards ? cTx.id : 'Block Reward';
