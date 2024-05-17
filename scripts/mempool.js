ce2802b38b00b2dd8a4d239e95ee3dba9567e0f7
Alessandro Rezzi <alessandrorezzi2000@gmail.com>
2024-05-17 17:17:41 +0200

    jsdoc

--- scripts/mempool.js
+++ scripts/mempool.js
@@ -175,6 +175,7 @@ export class Mempool {
      * @param {object} o - options
      * @param {number} [o.requirement] - A requirement to apply to all UTXOs. For example
      * `OutpointState.P2CS` will only return P2CS transactions.
+     * @param {number} [o.target] - Number of satoshis needed. The method will return early when the value of the UTXOs has been reached, plus a bit to account for change
      * By default it's MAX_SAFE_INTEGER
      * @param {boolean} [o.includeImmature] - If set to true immature UTXOs will be included
      * @param {number} [o.blockCount] - Current number of blocks
